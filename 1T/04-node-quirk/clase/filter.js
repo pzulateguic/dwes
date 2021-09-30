9be2166826a3c918c54b41f5cbce4a81b37493d7
pzulateguic <pablillozc@gmail.com>
2021-09-30 21:13:28 +0200

    subir ejercicios de clase 04

---
+++ 1T/04-node-quirk/clase/filter.js
@@ -0,0 +1,7 @@
+console.log("texto");
+
+ const elemento = [16, 35, 8, 22, 48].filter(essuficiente);
+ function essuficiente(elemento) {
+    return elemento>= 18;
+ }
+ console.log(elemento);
---
+++ 1T/04-node-quirk/clase/sumas.js
@@ -0,0 +1,4 @@
+let numeros = [1, 2, 3, 4, 5];
+let total = numeros.reduce((a, b) => a + b, 0);
+
+console.log(total);
