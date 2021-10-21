9bea681f47c7a3b7d21bb64ce62f1155e17c66df
pzulateguic <pablillozc@gmail.com>
2021-10-21 20:19:06 +0200

    no message

--- 1T/sprint-1/08-node-packages-manager/clase/index.js
+++
@@ -1,7 +0,0 @@
-const faker = require('faker');
-const chalk = require('chalk');
-
-const randomName = faker.name.findName();
-const randomColor = faker.commerce.color();
-
-console.log(chalk.keyword(randomColor)(randomName));
