06b6d7d280e7886b645c00cbfa694898980931de
pzulateguic <pablillozc@gmail.com>
2022-01-20 20:01:33 +0100

    no message

---
+++ 2T/index.js
@@ -0,0 +1,10 @@
+const protocol = 'mongodb+srv';
+const users = 'gabriada';
+const pass = 'gabriada';
+const host = 'cluster0.s48hv.mongodb.net';
+const dbName = 'test';
+
+    function conmect(){
+        await mongoose.connect(`~${protocol}://${user:$}`)
+
+    }
